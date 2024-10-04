4e3b6b00581ff6257bac0dcbefe701ed203a0e07
Joseph C. Caswell <josephcaswell@tpm.com>
2024-10-04 10:40:27 -0400

    Update query.js
    Minor changes

--- query/query.js
+++ query/query.js
@@ -57,7 +57,7 @@
  */
 
 // Enable console output to assist debugging
-const DEBUG_MODE = false
+const DEBUG_MODE = config.debug
 
 // Load settings from config file (configUser.js)
 const REQUIRE_NEW_SESSION = config.query.requireNewSession
@@ -211,10 +211,10 @@ class SessionManager {
 	async restartSession() {
 		try {
 			await client.logoutAllGroups()
-			await this.startSession()
 		} catch (error) {
 			debug(error, true)
 		}
+		await this.startSession()
 	}
 
 	/**
